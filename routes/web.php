ecad18858bc2526eeb25627ee59a2cdfe5b18d74
amirul akmal <amirul.akmal.putrajaya2003@gmail.com>
2025-12-02 11:47:23 +0800

    Added Login & Register Resource to web.php

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,12 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\guest\LoginController as Login;
+use App\Http\Controllers\guest\RegisterController as Register;
 
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::resource('login', Login::class);
+Route::resource('register', Register::class);
