22f6a8163f877047c527002b1c482270aca1b042
amirul akmal <amirul.akmal.putrajaya2003@gmail.com>
2025-12-05 00:45:57 +0800

    Added Dependacy SweetAlert2

--- resources/js/app.js
+++ resources/js/app.js
@@ -2,8 +2,10 @@ import './bootstrap';
 import '@fortawesome/fontawesome-free/js/all.min.js';
 import EasyMDE from 'easymde';
 import Tagify from '@yaireo/tagify';
-import { marked } from 'marked';
+import { marked } from 'marked';    
+import Swal from 'sweetalert2';
 
 window.EasyMDE = EasyMDE;
 window.Tagify = Tagify;
 window.marked = marked;
+window.Swal = Swal;
