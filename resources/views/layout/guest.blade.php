e2ffca9a9c057b80237760ceade3f06e5ffc7171
amirul akmal <amirul.akmal.putrajaya2003@gmail.com>
2025-12-07 10:02:22 +0800

    fix(guest@layout): remove sweetalert cdn

--- resources/views/layout/guest.blade.php
+++ resources/views/layout/guest.blade.php
@@ -36,9 +36,6 @@
         }
     </style>
 
-    <!-- SweetAlert2 -->
-    <script src="https://cdn.jsdelivr.net/npm/sweetalert2@11"></script>
-
     <!-- Tailwind CSS -->
     @vite(['resources/css/app.css', 'resources/js/app.js'])
 </head>
