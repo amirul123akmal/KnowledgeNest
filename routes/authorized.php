512549ac13fc3c6e0a1ce7968ee48f593765a373
amirul akmal <amirul.akmal.putrajaya2003@gmail.com>
2025-12-15 12:02:13 +0800

    feat(user@model): enable finding comments

--- routes/authorized.php
+++ routes/authorized.php
@@ -4,11 +4,13 @@ use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\auth\PostController as Post;
 use App\Http\Controllers\ProfileController;
 use App\Http\Controllers\admin\AdminController as Admin;
+use App\Http\Controllers\auth\UserDashboardController as User;
 
 Route::middleware('auth.login')->group(function () {
     Route::prefix('user')->group(function () {
         Route::resource('posts', Post::class)->except(['show', 'vote', 'like']);
         Route::resource('profile', ProfileController::class);
+        Route::resource('dashboard', User::class);
     });
 });
 Route::middleware('auth.admin')->group(function () {
