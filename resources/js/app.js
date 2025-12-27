559cb8d99ef5469f15c5c002d8d6a756f5335407
amirul akmal <amirul.akmal.putrajaya2003@gmail.com>
2025-12-27 08:12:00 +0800

    Ensure Alpine is working

--- resources/js/app.js
+++ resources/js/app.js
@@ -4,8 +4,12 @@ import EasyMDE from 'easymde';
 import Tagify from '@yaireo/tagify';
 import { marked } from 'marked';    
 import Swal from 'sweetalert2';
+import Alpine from 'alpinejs';
 
 window.EasyMDE = EasyMDE;
 window.Tagify = Tagify;
 window.marked = marked;
 window.Swal = Swal;
+window.Alpine = Alpine;
+Alpine.start();
+
