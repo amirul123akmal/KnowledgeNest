d8a5b256ee4a7389fddf6c203222b1af512f9eed
amirul akmal <amirul.akmal.putrajaya2003@gmail.com>
2025-12-27 08:13:12 +0800

    feat(Logging) : New response channel for the AI

--- config/logging.php
+++ config/logging.php
@@ -89,7 +89,7 @@ return [
             'handler_with' => [
                 'host' => env('PAPERTRAIL_URL'),
                 'port' => env('PAPERTRAIL_PORT'),
-                'connectionString' => 'tls://'.env('PAPERTRAIL_URL').':'.env('PAPERTRAIL_PORT'),
+                'connectionString' => 'tls://' . env('PAPERTRAIL_URL') . ':' . env('PAPERTRAIL_PORT'),
             ],
             'processors' => [PsrLogMessageProcessor::class],
         ],
@@ -121,6 +121,13 @@ return [
         'null' => [
             'driver' => 'monolog',
             'handler' => NullHandler::class,
+        ],
+
+        'response' => [
+            'driver' => 'single',
+            'path' => storage_path('logs/response.log'),
+            'level' => env('LOG_LEVEL', 'debug'),
+            'replace_placeholders' => true,
         ],
 
         'emergency' => [
